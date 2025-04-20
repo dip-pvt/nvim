394676a23a03bc2bbde4b580e65d88b65b219538
DIP <saumyadip.private@protonmail.com>
2025-04-20 20:14:09 +0530

    Update init.lua: add autosave

--- init.lua
+++ init.lua
@@ -1,1 +1,40 @@
 require("config.lazy")
+
+-- my addons
+-- 1. autosave after leave insert mode
+-- vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+--     pattern = "*",
+--     callback = function()
+--       if vim.bo.modified then
+--         vim.cmd("silent! write")
+--       end
+--     end,
+--   })
+  
+
+
+
+-- auto save after some times
+
+-- local save_timer = nil
+
+-- vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+--   pattern = "*",
+--   callback = function()
+--     if save_timer then
+--       save_timer:stop()
+--       save_timer:close()
+--     end
+
+--     save_timer = vim.loop.new_timer()
+--     save_timer:start(
+--       100,  -- 100 milliseconds delay
+--       0,
+--       vim.schedule_wrap(function()
+--         if vim.bo.modified then
+--           vim.cmd('silent! write')
+--         end
+--       end)
+--     )
+--   end,
+-- })

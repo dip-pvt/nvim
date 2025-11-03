bf3d3ff6939e48d00ccb8ac128dfc00250fa5933
DIP <saumyadip.private@protonmail.com>
2025-11-03 23:42:06 +0530

    Update tree-sitter.lua

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -8,27 +8,25 @@ return{
       configs.setup({
           ensure_installed = {
             "python",
-            "bash",
-            "rust", 
-            "c", 
+            "bash", 
             "lua", 
             "vim", 
             "vimdoc", 
-            "query",
+            "go",
             "json",
-            "csv",
-            "java",  
+            "csv", 
             "javascript", 
             "html" },
+          auto_install = true,  -- install perser when open new type of file
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },
+          indent = { enable = true }, -- indentation, off if make problem
           require'nvim-treesitter.configs'.setup {
             incremental_selection = {
               enable = true,
               keymaps = {
                 init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-                node_incremental = "<Enter>",
+                node_incremental = "<Enter>", -- select txts
                 scope_incremental = false,
                 node_decremental = "<Backspace>",  --need to think about (helping in delete)
               },

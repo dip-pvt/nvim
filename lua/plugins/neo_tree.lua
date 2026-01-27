bdd682e4fc323a778c690eb5eed19e099274f289
SAUMYADIP <sjana@localhost.localdomain>
2026-01-27 19:52:49 +0530

    update neotree '-' only use for active

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -1,48 +1,45 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    lazy = false, -- false: load autometic when start nvim
-    
-    -- ADD THIS CONFIG FUNCTION
-    config = function()
-      require("neo-tree").setup({
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false, -- false: load autometic when start nvim
 
-        window = {
-          -- Sets the width to 30% of the screen
-          width = 30,
-          
-          -- Or, use an absolute value (uncomment the line below)
-          -- width = 45, -- Sets the width to 45 columns
-          
-          -- This stops neo-tree from resizing itself
-          auto_resize = false
-        },
-        
-        -- This stops neo-tree from opening automatically when you start nvim
-        -- by hijacking the default file-explorer (netrw).
-        filesystem = {
-          hijack_netrw_behavior = "disabled", -- or "open_default"
-        },
-        
+		-- ADD THIS CONFIG FUNCTION
+		config = function()
+			require("neo-tree").setup({
 
-      })
+				window = {
+					-- Sets the width to 30% of the screen
+					width = 30,
 
-      -- KEYMAPS to open neo-tree
+					-- Or, use an absolute value (uncomment the line below)
+					-- width = 45, -- Sets the width to 45 columns
 
-      -- This is the specific command you were using
+					-- This stops neo-tree from resizing itself
+					auto_resize = false,
+				},
 
- 
-      vim.keymap.set("n", "-", ":Neotree toggle source=filesystem reveal=true position=right<CR>", { 
-        desc = "Neo-Tree toggle reveal file" })
+				-- This stops neo-tree from opening automatically when you start nvim
+				-- by hijacking the default file-explorer (netrw).
+				filesystem = {
+					hijack_netrw_behavior = "disabled", -- or "open_default"
+				},
+			})
 
-    end
-  }
+			-- KEYMAPS to open neo-tree
+
+			-- This is the specific command you were using
+
+			vim.keymap.set("n", "-", ":Neotree source=filesystem reveal=true position=right<CR>", {
+				desc = "Neo-Tree toggle reveal file",
+			})
+		end,
+	},
 }
 -- rename: r, add: file: a, folder: name/, remove:d, show/hode hidden files: H
 -- Using x (Cut) and p (Paste), Using m (Move)

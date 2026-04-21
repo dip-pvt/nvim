1db8668981065e56be8ad1f1cca8aeb0fd980255
SAUMYADIP <sjana@localhost.localdomain>
2026-04-21 12:29:37 +0530

    temp stop copilot and add show error option

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,51 +1,51 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			panel = {
-				enabled = true,
-				auto_refresh = false,
-				keymap = {
-					jump_prev = "[[",
-					jump_next = "]]",
-					accept = "<CR>",
-					refresh = "gr",
-					open = "<M-CR>",
-				},
-				layout = {
-					position = "bottom", -- | top | left | right | bottom |
-					ratio = 0.4,
-				},
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true, -- Set to true if you want suggestions as you type
-				hide_during_completion = true,
-				debounce = 75,
-				keymap = {
-					accept = "<M-l>", -- Alt + l to accept
-					accept_word = false,
-					accept_line = false,
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-			},
-			filetypes = {
-				yaml = false,
-				markdown = true,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
-			},
-			copilot_node_command = "node", -- Ensure this is version 22+
-			server_opts_overrides = {},
-		})
-	end,
+	-- "zbirenbaum/copilot.lua",
+	-- cmd = "Copilot",
+	-- event = "InsertEnter",
+	-- config = function()
+	-- 	require("copilot").setup({
+	-- 		panel = {
+	-- 			enabled = true,
+	-- 			auto_refresh = false,
+	-- 			keymap = {
+	-- 				jump_prev = "[[",
+	-- 				jump_next = "]]",
+	-- 				accept = "<CR>",
+	-- 				refresh = "gr",
+	-- 				open = "<M-CR>",
+	-- 			},
+	-- 			layout = {
+	-- 				position = "bottom", -- | top | left | right | bottom |
+	-- 				ratio = 0.4,
+	-- 			},
+	-- 		},
+	-- 		suggestion = {
+	-- 			enabled = true,
+	-- 			auto_trigger = true, -- Set to true if you want suggestions as you type
+	-- 			hide_during_completion = true,
+	-- 			debounce = 75,
+	-- 			keymap = {
+	-- 				accept = "<M-l>", -- Alt + l to accept
+	-- 				accept_word = false,
+	-- 				accept_line = false,
+	-- 				next = "<M-]>",
+	-- 				prev = "<M-[>",
+	-- 				dismiss = "<C-]>",
+	-- 			},
+	-- 		},
+	-- 		filetypes = {
+	-- 			yaml = false,
+	-- 			markdown = true,
+	-- 			help = false,
+	-- 			gitcommit = false,
+	-- 			gitrebase = false,
+	-- 			hgcommit = false,
+	-- 			svn = false,
+	-- 			cvs = false,
+	-- 			["."] = false,
+	-- 		},
+	-- 		copilot_node_command = "node", -- Ensure this is version 22+
+	-- 		server_opts_overrides = {},
+	-- 	})
+	-- end,
 }
